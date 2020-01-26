87c48b05bea4c5252cd5d3883643dc78cb12c14b
Chaminda Amarasinghe <chamindaamarasinghe@sidero.ie>
2020-01-26 23:22:09 +0000

    configurable delay

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-angular.module('myApp', []);
+myApp = angular.module('myApp', []);
+
+myApp.filter('range', function() {
+    return function(input, min, max, step) {
+        min = parseInt(min); //Make string input int
+        max = parseInt(max);
+        for (var i=min; i<max; i+=step)
+            input.push(i);
+        return input;
+    };
+});
